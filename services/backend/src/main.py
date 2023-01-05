13b2878f3b19ae90a72eea6a5704819d938530be
Ceasar <ceasarkwadwo@gmail.com>
2023-01-05 12:16:47 +0000

    project setup, models, schemas

--- services/backend/src/main.py
+++ services/backend/src/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from tortoise import Tortoise 
 
+from src.database.register import register_tortoise
+from src.database.config import TORTOISE_ORM   
+
+
+Tortoise.init_models(["src.database.models"], "models") 
 
 app = FastAPI()
 
@@ -12,7 +18,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+register_tortoise(app, config=TORTOISE_ORM, generate_schemas=False)
+
 
 @app.get("/")
 def home():
-    return "Hello, World!"
+    return "Welcome to Schedule SMS"
--- services/frontend/src/main.js
+++ services/frontend/src/main.js
@@ -8,7 +8,7 @@ import router from './router';
 const app = createApp(App);
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = 'http://localhost:5000/';  // the FastAPI backend
+axios.defaults.baseURL = 'http://localhost:5000/'; 
 
 app.use(router);
 app.mount("#app");
